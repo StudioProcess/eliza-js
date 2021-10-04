c4c2c49b540e7c55b3d6575f26d78a4852e709a7
Martin Grödl <martin@process.studio>
2021-10-04 17:25:19 +0200

    added initial tests

--- test/test.mjs
+++ test/test.mjs
@@ -1,11 +1,76 @@
 import tap from 'tap';
+import seedrandom from 'seedrandom';
+import { make_eliza } from '../index.mjs';
 
-// console.log(tap);
-import * as eliza from '../index.mjs';
+const rng = new seedrandom(0);
+/**
+ * Random number
+ * Call patterns:
+ *   rnd()     -> [0, 1)
+ *   rnd(a)    -> [0, a)
+ *   rnd(a, b) -> [a, b)
+ */
+export function rnd(a, b) {
+  let min = 0;
+  let max = 1;
+  if (b == undefined) {
+    if (a != undefined) max = a;
+  } else if (a != undefined) {
+    min = a;
+    max = b;
+  }
+  return min + rng() * (max-min);
+}
 
-// console.log(e);
+// let e;
+// 
+// tap.before(async () => {
+//   e = await make_eliza({ seed:0 });
+// });
 
-const e = await eliza.make_eliza();
 
-console.log(e.get_initial());
-console.log(e.is_quit());
+
+tap.test('assorted inputs', async t => {
+  const inputs = [
+    'Hello',
+    'I\'m fine',
+    'How are you?',
+    'Please, don\'t lecture me!',
+    'Bye',
+    'But I am so sad',
+    'I\'ve none',
+    'Because I want to'
+  ];
+  
+  const e = await make_eliza({ seed:0 });
+
+  for (const input of inputs) {
+    t.doesNotThrow(() => {
+      e.transform(input);
+    }, 'testing input: ' + input);
+  }
+});
+
+
+
+tap.test('random inputs', async t => {
+  const ROUNDS = 10;
+  const STR_LEN = 50;
+  const e = await make_eliza({ seed:0 });
+
+  function rnd_string(len = 1) {
+    let str = "";
+    for (let i=0; i<len; i++) {
+      let val = Math.floor(rnd(128));
+      str += String.fromCharCode(val);
+    }
+    return str;
+  }
+  
+  for (let i=0; i<ROUNDS; i++) {
+    let str = rnd_string(STR_LEN);
+    t.doesNotThrow(() => {
+      e.transform(str);
+    }, 'testing input: ' + str);
+  }
+});
